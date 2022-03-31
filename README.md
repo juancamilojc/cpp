e65ffd27d4a91030f16a0463aa0b92f38c2311a0
Juan Camilo <82708214+juancamilojc@users.noreply.github.com>
2022-03-31 08:22:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cpp
+ Estudos de C++ e Resoluções de Exercícios do Beecrowd
---
+++ beecrowd/iniciante/1005/bee1005.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    double A, B, media;
+
+    cin >> A;
+    cin >> B;
+
+    media = ((A * 3.5) + (B * 7.5)) / 11;
+
+    cout << fixed << setprecision(5);
+    cout << "MEDIA = " << media << endl;
+
+    return 0;
+}
